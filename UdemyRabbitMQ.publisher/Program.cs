93ab09745de51316db9f7ae495a639feaa1cf936
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-26 01:25:37 +0300

    Mesajları kalıcı hale getirmek

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -36,7 +36,7 @@ namespace UdemyRabbitMQ.publisher
 
             var properties = channel.CreateBasicProperties();
             properties.Headers = headers;
-            
+            properties.Persistent = true;
 
 
             channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes("header mesajım"));
