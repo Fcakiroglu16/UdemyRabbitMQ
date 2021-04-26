efb723d223894b6ea4e1ec1f7ceecac517f8ac7a
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-27 01:28:42 +0300

    1. Adım :Projenin oluşturulması

---
+++ UdemyRabbitMQWeb.Watermark/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyRabbitMQWeb.Watermark.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products  { get; set; }
+    }
+}
---
+++ UdemyRabbitMQWeb.Watermark/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyRabbitMQWeb.Watermark.Models
+{
+    public class Product
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Column(TypeName ="decimal(18,2")]
+        public decimal Price { get; set; }
+        [Range(1,100)]
+        public int Stock { get; set; }
+        [StringLength(100)]
+        public string PictureUrl { get; set; }
+    }
+}
