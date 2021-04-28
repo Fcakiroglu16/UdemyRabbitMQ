abe1c7f0798e50d64d879624d151038e684af59b
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-28 09:06:27 +0300

    Senaryo 2 : 8. Adım :FilesController  >  Upload Method

---
+++ UdemRabbitMQWeb.ExcelCreate/Controllers/FilesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemRabbitMQWeb.ExcelCreate.Models;
+
+namespace UdemRabbitMQWeb.ExcelCreate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public FilesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file,int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+
+
+            var userFile = await  _context.UserFiles.FirstAsync(x => x.Id == fileId);
+
+            var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+
+
+
+            using FileStream stream = new(path, FileMode.Create);
+
+            await file.CopyToAsync(stream);
+
+
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = filePath;
+            userFile.FileStatus = FileStatus.Completed;
+
+            await _context.SaveChangesAsync();
+            //SignalR notification oluşturulacak
+            return Ok();
+        }
+    }
+}
