7d0ce07ab4ebbdf7bc8d7027de1f11669ff33c37
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-28 07:56:00 +0300

    Senaryo 2 :  3. Adım :ProductController >  CreateProductExcel() ve Files() methodlarının oluşturulması

--- UdemRabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
+++ UdemRabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemRabbitMQWeb.ExcelCreate.Models;
 
 namespace UdemRabbitMQWeb.ExcelCreate.Controllers
 {
@@ -11,9 +14,54 @@ namespace UdemRabbitMQWeb.ExcelCreate.Controllers
     [Authorize]
     public class ProductController : Controller
     {
+
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProductController(AppDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CreateProductExcel()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var fileName = $"product-excel-{Guid.NewGuid().ToString().Substring(1, 10)}";
+
+            UserFile userfile = new()
+            {
+                UserId = user.Id,
+                FileName = fileName,
+                FileStatus = FileStatus.Creating
+            };
+
+          await  _context.UserFiles.AddAsync(userfile);
+
+
+            await _context.SaveChangesAsync();
+            //rabbitMQ'ya messaj gönder
+            TempData["StartCreatingExcel"] = true;
+
+            return RedirectToAction(nameof(Files));
+
+
+        }
+
+        public async Task<IActionResult> Files()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+
+
+
+            return View( await _context.UserFiles.Where(x=>x.UserId==user.Id).ToListAsync());
         }
     }
 }
