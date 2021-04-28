2a297e48f0f40de90848d3bb0330c9218ca0677c
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-28 08:51:14 +0300

    Senaryo 2 : 7. Adım :Database first

---
+++ FileCreateWorkerService/Models/AdventureWorks2019Context.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace FileCreateWorkerService.Models
+{
+    public partial class AdventureWorks2019Context : DbContext
+    {
+        public AdventureWorks2019Context()
+        {
+        }
+
+        public AdventureWorks2019Context(DbContextOptions<AdventureWorks2019Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product", "Production");
+
+                entity.HasComment("Products sold or used in the manfacturing of sold products.");
+
+                entity.HasIndex(e => e.Name, "AK_Product_Name")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.ProductNumber, "AK_Product_ProductNumber")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Rowguid, "AK_Product_rowguid")
+                    .IsUnique();
+
+                entity.Property(e => e.ProductId)
+                    .HasColumnName("ProductID")
+                    .HasComment("Primary key for Product records.");
+
+                entity.Property(e => e.Class)
+                    .HasMaxLength(2)
+                    .IsFixedLength(true)
+                    .HasComment("H = High, M = Medium, L = Low");
+
+                entity.Property(e => e.Color)
+                    .HasMaxLength(15)
+                    .HasComment("Product color.");
+
+                entity.Property(e => e.DaysToManufacture).HasComment("Number of days required to manufacture the product.");
+
+                entity.Property(e => e.DiscontinuedDate)
+                    .HasColumnType("datetime")
+                    .HasComment("Date the product was discontinued.");
+
+                entity.Property(e => e.FinishedGoodsFlag)
+                    .IsRequired()
+                    .HasDefaultValueSql("((1))")
+                    .HasComment("0 = Product is not a salable item. 1 = Product is salable.");
+
+                entity.Property(e => e.ListPrice)
+                    .HasColumnType("money")
+                    .HasComment("Selling price.");
+
+                entity.Property(e => e.MakeFlag)
+                    .IsRequired()
+                    .HasDefaultValueSql("((1))")
+                    .HasComment("0 = Product is purchased, 1 = Product is manufactured in-house.");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())")
+                    .HasComment("Date and time the record was last updated.");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasComment("Name of the product.");
+
+                entity.Property(e => e.ProductLine)
+                    .HasMaxLength(2)
+                    .IsFixedLength(true)
+                    .HasComment("R = Road, M = Mountain, T = Touring, S = Standard");
+
+                entity.Property(e => e.ProductModelId)
+                    .HasColumnName("ProductModelID")
+                    .HasComment("Product is a member of this product model. Foreign key to ProductModel.ProductModelID.");
+
+                entity.Property(e => e.ProductNumber)
+                    .IsRequired()
+                    .HasMaxLength(25)
+                    .HasComment("Unique product identification number.");
+
+                entity.Property(e => e.ProductSubcategoryId)
+                    .HasColumnName("ProductSubcategoryID")
+                    .HasComment("Product is a member of this product subcategory. Foreign key to ProductSubCategory.ProductSubCategoryID. ");
+
+                entity.Property(e => e.ReorderPoint).HasComment("Inventory level that triggers a purchase order or work order. ");
+
+                entity.Property(e => e.Rowguid)
+                    .HasColumnName("rowguid")
+                    .HasDefaultValueSql("(newid())")
+                    .HasComment("ROWGUIDCOL number uniquely identifying the record. Used to support a merge replication sample.");
+
+                entity.Property(e => e.SafetyStockLevel).HasComment("Minimum inventory quantity. ");
+
+                entity.Property(e => e.SellEndDate)
+                    .HasColumnType("datetime")
+                    .HasComment("Date the product was no longer available for sale.");
+
+                entity.Property(e => e.SellStartDate)
+                    .HasColumnType("datetime")
+                    .HasComment("Date the product was available for sale.");
+
+                entity.Property(e => e.Size)
+                    .HasMaxLength(5)
+                    .HasComment("Product size.");
+
+                entity.Property(e => e.SizeUnitMeasureCode)
+                    .HasMaxLength(3)
+                    .IsFixedLength(true)
+                    .HasComment("Unit of measure for Size column.");
+
+                entity.Property(e => e.StandardCost)
+                    .HasColumnType("money")
+                    .HasComment("Standard cost of the product.");
+
+                entity.Property(e => e.Style)
+                    .HasMaxLength(2)
+                    .IsFixedLength(true)
+                    .HasComment("W = Womens, M = Mens, U = Universal");
+
+                entity.Property(e => e.Weight)
+                    .HasColumnType("decimal(8, 2)")
+                    .HasComment("Product weight.");
+
+                entity.Property(e => e.WeightUnitMeasureCode)
+                    .HasMaxLength(3)
+                    .IsFixedLength(true)
+                    .HasComment("Unit of measure for Weight column.");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- FileCreateWorkerService/Program.cs
+++ FileCreateWorkerService/Program.cs
@@ -1,4 +1,6 @@
+using FileCreateWorkerService.Models;
 using FileCreateWorkerService.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,12 @@ namespace FileCreateWorkerService
 
 
                     IConfiguration Configuration = hostContext.Configuration;
+
+                    services.AddDbContext<AdventureWorks2019Context>(options =>
+                    {
+                        options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+                    });
+
 
                     services.AddSingleton<RabbitMQClientService>();
                     services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")), DispatchConsumersAsync = true });
