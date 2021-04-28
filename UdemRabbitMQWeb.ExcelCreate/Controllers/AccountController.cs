60c558d323cfe35dedbe2149b0d1032af77a690d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-28 06:09:14 +0300

    Senaryo 2:   2. Adım :AccountController > Login sayfasının oluşturulması

---
+++ UdemRabbitMQWeb.ExcelCreate/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemRabbitMQWeb.ExcelCreate.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email,string Password)
+        {
+
+            var hasUser = await _userManager.FindByEmailAsync(Email);
+
+
+            if(hasUser==null)
+            {
+                return View();
+            }
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, Password, true, false);
+
+            if (!signInResult.Succeeded)
+            {
+
+                return View();
+            }
+
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ UdemRabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemRabbitMQWeb.ExcelCreate.Controllers
+{
+
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
